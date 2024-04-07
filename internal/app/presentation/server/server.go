bd044e3c9271a46312aca112004c79042762b2c5
yuuLab <yuto.yoshida0118@gmail.com>
2024-04-07 11:28:01 +0900

    add: graceful shutdown

--- internal/app/presentation/server/server.go
+++ internal/app/presentation/server/server.go
@@ -1,9 +1,14 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/labstack/echo"
 	echomiddleware "github.com/labstack/echo/middleware"
@@ -27,5 +32,22 @@ func Run() {
 		port = "8080"
 		slog.Warn(fmt.Sprintf("Defaulting to port %s", port))
 	}
-	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", port)))
+	go func() {
+		if err := e.Start(fmt.Sprintf(":%s", port)); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("Shutting down the server")
+		}
+	}()
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	defer stop()
+	<-ctx.Done()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	slog.Info("server shutdown completed.")
 }
